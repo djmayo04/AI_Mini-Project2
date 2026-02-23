adc5f889df53ee1122528aee9325dd9283675c4c
djmayo04 <119097831+djmayo04@users.noreply.github.com>
2026-02-23 03:24:21 -0500

    Update agent.py
    Overwrite this; Minimal version of agent file with example logic for the example tool.

--- agent.py
+++ agent.py
@@ -1,1 +1,20 @@
+from llm_helper import get_completion
+from tools import search_documents
 
+def run_agent(user_input):
+
+    decision_prompt = f"""
+Decide ONE:
+- call_tool
+- answer_directly
+
+User input: {user_input}
+"""
+
+    decision = get_completion(decision_prompt)
+
+    if "call_tool" in decision:
+        tool_result = search_documents.invoke(user_input)
+        return tool_result
+
+    return get_completion(user_input)
