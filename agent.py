20b31aaf358329a8fca3f9438a9b475b6309d200
djmayo04 <119097831+djmayo04@users.noreply.github.com>
2026-02-23 03:43:28 -0500

    Update agent.py
    Still a framework. Added memory

--- agent.py
+++ agent.py
@@ -1,20 +1,42 @@
 from llm_helper import get_completion
 from tools import search_documents
+from memory import Memory
 
-def run_agent(user_input):
+memory = Memory()
+
+def run_agent(user_input: str):
+    memory.add_user(user_input)
 
     decision_prompt = f"""
-Decide ONE:
+You are a rational academic assistant.
+
+Decide ONE action:
 - call_tool
 - answer_directly
 
 User input: {user_input}
+Respond with only the action name.
 """
 
     decision = get_completion(decision_prompt)
 
     if "call_tool" in decision:
+        
         tool_result = search_documents.invoke(user_input)
-        return tool_result
+        
+        final_prompt = f"""
+Use the following tool result to answer the user:
 
-    return get_completion(user_input)
+{tool_result}
+
+User question:
+{user_input}
+"""
+        response = get_completion(final_prompt)
+
+    else:
+        response = get_completion(user_input)
+
+   memory.add_assistant(response)
+
+    return response
