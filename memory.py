4da41d1f6494f8a1b6ad3ee9176569206048aef3
djmayo04 <119097831+djmayo04@users.noreply.github.com>
2026-02-23 03:45:51 -0500

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,22 @@
+class Memory:
 
+    def __init__(self):
+        self.history = []
+
+    def add_user(self, text):
+        self.history.append({
+            "role": "user",
+            "content": text
+        })
+
+    def add_assistant(self, text):
+        self.history.append({
+            "role": "assistant",
+            "content": text
+        })
+
+    def get_history(self):
+        return self.history
+
+   def clear(self):
+        self.history = []
