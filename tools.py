b2a55a425d7b0b91465f930192107a77f2df5ade
djmayo04 <119097831+djmayo04@users.noreply.github.com>
2026-02-24 12:22:19 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -8,5 +8,4 @@ def set_vectorstore(vs):
 
 @tool
 def search_docs(query: str) -> str:
-    """Searches for documents based on the provided query."""
-    return "Pretend this searched the syllabus."
+    return vectorstore.similary_search(query, k=4)
