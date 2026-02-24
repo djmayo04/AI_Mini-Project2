4394a1273353dd9c0c978dec278119c2c6fbc3e3
Christopher Ponds <87038488+ShowMeYourLegos96@users.noreply.github.com>
2026-02-24 14:14:07 -0500

    Update search_documents.py

--- search_documents.py
+++ search_documents.py
@@ -1,6 +1,20 @@
-import glob
+#import glob
 
 #returns the list of files that match a specified pattern
-files = glob.glob('*.txt') 
-for files in files:
-    print(files)
+#files = glob.glob('*.txt') 
+#for files in files:
+    #print(files)
+import glob
+import pathlib
+import os
+
+doc = input("Enter a document: ")
+DL = 'C:/User/Chrom/Downloads'
+for relPath,dirs,files in os.walk(DL):
+ fullPath = os.path.join(DL,relPath,doc)
+ print(fullPath)
+ if doc in files:
+    docu = open(doc, "r")
+    content = docu.read()
+    print(content)
+    docu.close()
