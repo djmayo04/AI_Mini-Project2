0c9ecb4a2db0c489927d299895ec8bb09786b891
K. Holbert <144261015+kholbyjack@users.noreply.github.com>
2026-02-23 23:33:01 -0500

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,8 +1,14 @@
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
 def get_completion(prompt, temperature=0):
     response = llm.invoke([HumanMessage(content=prompt)])
     return response.content
+
+def chunk_split(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=25)
+    chunks = text_splitter.split_text(text)
+    return chunks
