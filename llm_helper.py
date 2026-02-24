1e2fd473af2509ec8ce8c74255afb38fc546f5f1
djmayo04 <119097831+djmayo04@users.noreply.github.com>
2026-02-24 00:42:02 -0500

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -5,10 +5,10 @@ from langchain_core.documents import Document
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-def get_completion(prompt, temperature=0):
+def get_completion(prompt: str, temperature:float = 0):
     response = llm.invoke([HumanMessage(content=prompt)])
     return response.content
 
-def chunk_split(text):
+def chunk_split(documents):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=25)
     return text_splitter.split_documents(documents)
